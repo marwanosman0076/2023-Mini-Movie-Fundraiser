5216f9b7cd56c7dc5ee796097645b2223378a1a5
Marwan <89755615+marwanosman0076@users.noreply.github.com>
2023-03-14 10:06:06 +1300

    Add files via upload

--- 08_ticket_cost.py
+++ 08_ticket_cost.py
@@ -1,4 +1,17 @@
+# Next Steps
+# Add currency formatting & Headings
+# integrate this component with the base component
+
+# PS: In base component remember to calculate surcharge
+# once payment method has been chosen!!
+
 import pandas
+
+
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
+
 
 # dictionaries to hold ticket details
 all_names = ["a", "b", "c", "d", "e"]
@@ -23,6 +36,9 @@ mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
 # Calculate ticket and profit for each ticket
 total = mini_movie_frame['Total'].sum()
 profit = mini_movie_frame['Profit'].sum()
+
+# Currency Formatting (uses currency function)
+add_dollars = {'Ticket Price', 'Surcharge', 'Total', 'Profit'}
 
 # output table with ticket data
 print(mini_movie_frame)
